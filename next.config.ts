af3014b8e33a8696894538e647e4c95bb3b5c440
VVoronkov <voronglass84@gmail.com>
2025-03-26 18:45:49 +0300

    Change config to deploy on GH pages

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
     domains: ['fakestoreapi.com', 'media.istockphoto.com'],
+    unoptimized: true,
   },
   async redirects() {
     return [
@@ -14,6 +18,9 @@ const nextConfig: NextConfig = {
       },
     ];
   },
+  distDir: 'out',
+  basePath: isProd ? '/testtask_alfa' : '',
+  assetPrefix: isProd ? '/testtask_alfa/' : '',
 };
 
 export default nextConfig;
