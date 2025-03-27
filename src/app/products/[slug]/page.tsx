da7d3cd98db74ae800cb568de3f72bcd7dd09614
VVoronkov <voronglass84@gmail.com>
2025-03-27 11:29:59 +0300

    Fix error on ProductPage when deploy app on github pages

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -4,10 +4,15 @@ import { notFound } from "next/navigation";
 import Product from '@/app/types/Product';
 import ProductClient from './ProductClient';
 
-export default async function ProductPage({ params }: { params: { slug: string | number } }) {
-  const { slug } = await params;
+type ProductPageProps = {
+  params: { slug: string };
+};
 
-  const product = await fetch(`https://fakestoreapi.com/products/${slug}`)
+export default async function ProductPage({ params }: ProductPageProps) {
+  // const { slug } = await params;
+  if (!params.slug) return notFound(); 
+
+  const product = await fetch(`https://fakestoreapi.com/products/${params.slug}`)
     .then((res) => res.ok ? res.json() : null)
     .catch(() => null);
 
